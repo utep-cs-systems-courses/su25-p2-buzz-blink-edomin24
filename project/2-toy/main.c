1f5b6b86220e2ea0a7206f6b6a43d792cac35f68
edomin24 <edominguez28@miners.utep.edu>
2025-07-18 23:45:53 -0600

    Got Led on but switches still unresponsive

--- project/2-toy/main.c
+++ project/2-toy/main.c
@@ -1,25 +1,13 @@
 #include <msp430.h>
 #include "switch.h"
+#include "libTimer.h"
+#include "led.h"
 
 void main(void) {
-  WDTCTL = WDTPW | WDTHOLD;       // Stop watchdog timer
-
   // Set P1.0 (Red LED) as output
   P1DIR |= BIT0;
-  P1OUT &= ~BIT0;                 // Start with LED off
-  switch_init();                  // Initialize switches
-  __enable_interrupt();           // Enable global interrupts
-
-  while (1) {
-    // Check global state set in switch_interrupt_handler
-    if (switch_state_changed) {
-      switch_state_changed = 0;
-      if (switch1_state || switch2_state || switch3_state || switch4_state) {
-	P1OUT |= BIT0;      // Turn ON LED if any switch is pressed
-      } else {
-	P1OUT &= ~BIT0;     // Turn OFF LED otherwise
-      }
-    }
-  }
+  P1OUT |= BIT0;                 // Start with LED off
+  
+  or_sr(0x18);
 }
 
